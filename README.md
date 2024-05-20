c736affbb1995ad8c1f348ac383db56c48f8bd38
Jishan Hossain <imranislamjishan200@gmail.com>
2024-05-20 20:47:16 +0600

    basic server serup

---
+++ README.md
@@ -0,0 +1,1 @@
+# E-Shop---Assignment-2
---
+++ src/app.ts
@@ -0,0 +1,35 @@
+import express, { NextFunction, Request, Response } from "express";
+import cors from "cors";
+
+const app = express();
+
+//parset
+app.use(express.json());
+app.use(cors());
+
+//Application Routes
+// app.use("/api/v1/students", StudentRoutes);
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("E-Shop Server Running!");
+});
+
+//route get errpr
+app.all("*", (req: Request, res: Response) => {
+  res.status(404).json({
+    success: false,
+    message: "Route Not Found",
+  });
+});
+
+//global error handler
+app.use((error: any, req: Request, res: Response, next: NextFunction) => {
+  if (error) {
+    res.status(400).json({
+      success: false,
+      message: error.message,
+    });
+  }
+});
+
+export default app;
