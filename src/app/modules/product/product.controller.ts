1f9716c6fc5c8742fdfebd8dd95f1c64dd6546cd
Jishan Hossain <imranislamjishan200@gmail.com>
2024-05-20 21:44:10 +0600

    post api create

--- src/app/modules/product/product.controller.ts
+++ src/app/modules/product/product.controller.ts
@@ -1,6 +1,23 @@
 import { Request, Response } from "express";
+import { ProductService } from "./product.service";
 
-const createProduct = async (req: Request, res: Response) => {};
+const createProduct = async (req: Request, res: Response) => {
+  try {
+    const productData = req.body;
+    const result = await ProductService.createProductIntoDb(productData);
+
+    res.status(201).json({
+      success: true,
+      message: "Product created successfully!",
+      data: result,
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: true,
+      message: error.message,
+    });
+  }
+};
 
 export const ProductController = {
   createProduct,
--- src/app/modules/product/product.service.ts
+++ src/app/modules/product/product.service.ts
@@ -1,4 +1,10 @@
-const createProductIntoDb = async (item) => {};
+import { IProduct } from "./product.interface";
+import { Product } from "./product.model";
+
+const createProductIntoDb = async (item: IProduct) => {
+  const result = await Product.create(item);
+  return result;
+};
 
 export const ProductService = {
   createProductIntoDb,
